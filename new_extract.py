26e76f26d799af17ec83f2a10e936f801d6ccf48
Taha Çakır <77748816+Taha-Cakir@users.noreply.github.com>
2023-05-13 01:16:08 +0300

    Update new_extract.py

--- new_extract.py
+++ new_extract.py
@@ -1,5 +1,10 @@
 from airflow.decorators import dag, task
 from pendulum import datetime
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.amazon.aws.hooks.base_aws import AwsGenericHook
 
 import requests
 import json
